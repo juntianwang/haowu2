ac6538d674e016e660c438c02c9c51544559bb64
juntianwang <liujunqi162@163.com>
2017-07-23 17:49:28 +0800

    json
    json

--- haowu/src/components/my/mychild/dome.vue
+++ haowu/src/components/my/mychild/dome.vue
@@ -1,19 +1,38 @@
 <template>
-<mt-switch v-model="value">{{checked.Boolean}}</mt-switch>
+<ul
+  v-infinite-scroll="loadMore"
+  infinite-scroll-disabled="loading"
+  infinite-scroll-distance="10">
+  <li v-for="item in list" style="height: 100px; border-bottom: 5px solid black; background: red;">{{ item }}</li>
+</ul>
 
 </template>
 
 <script>
-import { CellSwipe } from 'mint-ui';
-import { Header } from 'mint-ui';
-import { Search } from 'mint-ui';
-import { Switch } from 'mint-ui';
+import { InfiniteScroll } from 'mint-ui';
+import data from '../../../../test/shopping.json';
+
 export default {
 		data() {
 			return {
-
+				list:[1,2,3,4,5,6,7]
 			}
 		},
+		methods:{
+			loadMore() {
+				  this.loading = true;
+				  setTimeout(() => {
+				    let last = this.list[this.list.length - 1];
+				    for (let i = 1; i <= 10; i++) {
+				      this.list.push(last + i);
+				    }
+				    this.loading = true;
+				  }, 2500);
+				}
+		},
+		mounted (){
+			console.log(data.shopping.likeList.length)
+		}	
 	}
 </script>
 
